f77a0db68a8017b861efa766b1c3cf5633ba16f2
Mih@s` <mihas240185@gmail.com>
2022-11-30 10:56:03 +0300

    start_files

---
+++ hello_django/hello_django/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def about(request):
+    return render(request, 'about.html', {'gretting':'Hello'})
+
+
+def home(request):
+    return HttpResponse('This is home page!!!')
+
+
