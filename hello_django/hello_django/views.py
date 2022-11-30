835eeb54251000b239cb4fc86a09dbe0b0229019
Mih@s` <mihas240185@gmail.com>
2022-11-30 17:02:59 +0300

    start_files

--- hello_django/hello_django/views.py
+++ hello_django/hello_django/views.py
@@ -1,12 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django import forms
 
 
 def about(request):
-    return render(request, 'about.html', {'gretting':'Hello'})
+    return render(request, 'about.html', {'gretting': 'Hello'})
+
+
+class UserForm(forms.Form):
+    # name = forms.CharField(label="Имя")
+    comment = forms.CharField(label="", widget=forms.Textarea)
+
+
+comment_form = UserForm
 
 
 def home(request):
-    return HttpResponse('This is home page!!!')
-
-
+    return render(request, 'home.html', {'text_area': comment_form})
